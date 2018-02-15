b64d3db32c4eccf5c8dac0060956aa81870da851
LeeSD90 <lee.doyle90@gmail.com>
2018-02-15 10:57:07 +0100

    Added bubble_sort_by solution which sorts an array using bubble sort but accepts a block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,7 +15,22 @@ def bubble_sort(arg)
 end
 
 def bubble_sort_by(arg)
-
+	again = true
+	while again
+		again = false
+		i = 0
+		while i < (arg.length-1)
+			if yield(arg[i], arg[i+1]) > 1
+				arg[i], arg[i+1] = arg[i+1], arg[i]
+				again = true
+			end
+			i+=1
+		end
+	end
+	return arg
 end
 
 print bubble_sort([1,8,82,-1,0,-32,2,3,1])
+print bubble_sort_by(["hi","hello","hey"]) { |left,right|
+  left.length - right.length
+}
